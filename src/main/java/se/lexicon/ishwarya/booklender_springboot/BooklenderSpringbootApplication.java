993a320bbdfd9c7233b578d67661a7f0334ba42c
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-02-09 10:14:44 +0100

    Initial commit

---
+++ src/main/java/se/lexicon/ishwarya/booklender_springboot/BooklenderSpringbootApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.ishwarya.booklender_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklenderSpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklenderSpringbootApplication.class, args);
+    }
+
+}
