2161be58d128d41b71d74c3c9022ba68aa56b8e6
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-02-10 15:31:52 +0100

    Part2 completed

--- src/main/java/se/lexicon/ishwarya/booklender_springboot/repository/LoanRepository.java
+++ src/main/java/se/lexicon/ishwarya/booklender_springboot/repository/LoanRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 @Repository
 public interface LoanRepository extends CrudRepository<Loan,Long> {
     List<Loan> findByUserId(Long userId);
-    List<Loan> findByBookId(Long bookId);
+    List<Loan> findByBookId(int bookId);
     @Query("SELECT l FROM Loan l WHERE l.concluded = :concluded")
     List<Loan> findByConcludedStatus(@Param("concluded") boolean concluded);
 
