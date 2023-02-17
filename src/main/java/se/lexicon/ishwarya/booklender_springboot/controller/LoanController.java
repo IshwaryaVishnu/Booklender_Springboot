decf0d00c73362f793f48cf17fd789ed834cf4f2
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2023-02-17 15:46:48 +0100

    Part4

---
+++ src/main/java/se/lexicon/ishwarya/booklender_springboot/controller/LoanController.java
@@ -0,0 +1,44 @@
+package se.lexicon.ishwarya.booklender_springboot.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import se.lexicon.ishwarya.booklender_springboot.dto.LoanDto;
+import se.lexicon.ishwarya.booklender_springboot.service.LoanService;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/loans")
+public class LoanController {
+
+    @Autowired
+    LoanService loanService;
+
+    @GetMapping("/")
+    public ResponseEntity<List<LoanDto>> find(@RequestParam(value = "userId", required = false) Integer userId,
+                                              @RequestParam(value = "bookId", required = false) Integer bookId,
+                                              @RequestParam(value = "status", required = false) String status) {
+        List<LoanDto> loans = loanService.findAll();
+        return ResponseEntity.ok(loans);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LoanDto> findById(@PathVariable("id") Long id) {
+        LoanDto loan = loanService.findById(id);
+        return ResponseEntity.ok(loan);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<LoanDto> create(@RequestBody LoanDto loanDto) {
+        LoanDto createdLoan = loanService.create(loanDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdLoan);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<LoanDto> update( @RequestBody LoanDto loanDto) {
+        LoanDto updatedLoan = loanService.update(loanDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(updatedLoan);
+    }
+    }
